063495b98c490ee27a8769f7afc2a213bf8b42ea
unix <unix.bio@gmail.com>
2021-03-19 18:44:14 +0800

    fix: fix the name of welcome print

--- src/utils/print.ts
+++ src/utils/print.ts
@@ -1,33 +1,33 @@
-import chalk from 'chalk'
-import * as spinner from './spinner'
+import chalk from "chalk";
+import * as spinner from "./spinner";
 
 export const logColor = (text: string): string => {
-  return chalk.hex('#bdbdbd')(text)
-}
+  return chalk.hex("#bdbdbd")(text);
+};
 
 export const cyanColor = (text: string): string => {
-  return chalk.cyanBright(text)
-}
+  return chalk.cyanBright(text);
+};
 
 export const dangerColor = (text: string): string => {
-  return chalk.redBright(text)
-}
+  return chalk.redBright(text);
+};
 
 export const catchErr = (err: Error): void => {
-  const msg = err.message || `${err}`
-  spinner.fail()
-  console.log(dangerColor(`> ${msg}`))
-  process.exit(1)
-}
+  const msg = err.message || `${err}`;
+  spinner.fail();
+  console.log(dangerColor(`> ${msg}`));
+  process.exit(1);
+};
 
 export const promptText = (text: string): string => {
-  text = text.replace('project name', chalk.bold('project name'))
-  return logColor(text)
-}
+  text = text.replace("project name", chalk.bold("project name"));
+  return logColor(text);
+};
 
 export const welcome = (): void => {
-  const name = chalk.hex('#FEFEFE').bold('koa-ts')
-  const text = chalk.hex('#F0F0F0')(`  Welcome to ${name} installer`)
-  console.log(text)
-  console.log('')
-}
+  const name = chalk.hex("#FEFEFE").bold("unix.bio");
+  const text = chalk.hex("#F0F0F0")(`  Welcome to ${name} installer`);
+  console.log(text);
+  console.log("");
+};
